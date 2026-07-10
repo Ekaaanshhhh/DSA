59e96629bc65c981bbf8066af9cc91e18f200aae
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-07-10 08:12:54 +0530

    Time: 21 ms (36.16%), Space: 53.6 MB (82.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -14,10 +14,34 @@ class Solution {
         }
     }
     public int numDistinct(String s, String t) {
-        int dp[][] = new int[s.length()][t.length()];
+        // int dp[][] = new int[s.length()][t.length()];
+        // for(int i=0;i<dp.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return helper(s,t,s.length()-1,t.length()-1,dp);
+
+        int dp[][] = new int[s.length()+1][t.length()+1];
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return helper(s,t,s.length()-1,t.length()-1,dp);
+
+        dp[0][0]=1;
+        for(int j=1;j<dp[0].length;j++){
+            dp[0][j]=0;
+        }
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j]+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 }
