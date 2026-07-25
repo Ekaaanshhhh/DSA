bdd95cb6726808a732927a69dd00315221cb2baa
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-07-25 12:48:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -0,0 +1,48 @@
+class Solution {
+    class Pair{
+        int child;
+        int parent;
+        public Pair(int c,int p){
+            child = c;
+            parent=p;
+        }
+    }
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj,int i,boolean vis[]){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,-1));
+        vis[i]=true;
+        while(!q.isEmpty()){
+            Pair p = q.remove();
+            int node = p.child;
+            for(int j:adj.get(node)){
+                if(!vis[j]){
+                    vis[j]=true;
+                    q.add(new Pair(j,node));
+                }
+                else if(j!=p.parent && vis[j])return true;
+            }
+        }
+        return false;
+    }
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int i=0;i<edges.length;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        boolean vis[] = new boolean[V];
+        Arrays.fill(vis,false);
+        for(int i=0;i<vis.length;i++){
+            if(!vis[i]){
+                if(bfs(adj,i,vis))return true;
+            }
+        }
+        return false;
+    }
+}
