8d5ce4f4183595f29b62d10b55035c8e062bc65f
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-16 12:00:05 +0530

    Time: 2 ms (17.08%), Space: 46.6 MB (32.71%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -5,7 +5,23 @@ class Solution {
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return helper(prices,0,0,dp);
+        // return helper(prices,0,0,dp);
+
+        for(int i=0;i<dp[0].length;i++){
+            dp[prices.length][i]=0;
+        }
+
+        for(int index=dp.length-2;index>=0;index--){
+            for(int buy=1;buy>=0;buy--){
+                if(buy==1){
+                    dp[index][buy]=Math.max(prices[index]+dp[index+1][0],dp[index+1][buy]);
+                }
+                 else{
+                    dp[index][buy]=Math.max(-prices[index]+dp[index+1][1],dp[index+1][buy]);
+                 }
+            }
+        }
+        return dp[0][0];
     }
     public int helper(int prices[],int index,int buy,int dp[][]){
         if(index==prices.length){
