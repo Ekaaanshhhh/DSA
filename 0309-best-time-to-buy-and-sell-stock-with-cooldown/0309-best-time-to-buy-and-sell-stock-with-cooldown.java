f561421c9134cae840dd351217eef1e4c092721c
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-16 12:28:34 +0530

    Time: 1 ms (69.02%), Space: 43.5 MB (64.59%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,18 +1,19 @@
 class Solution {
-    public int helper(int i,int prices[],int buy,int dp[][]){
-        if(i>=prices.length)return 0;
-        if(dp[i][buy]!=-1)return dp[i][buy];
-        if(buy==1){
-            return dp[i][buy]=Math.max(-prices[i]+helper(i+1,prices,0,dp),helper(i+1,prices,1,dp));
-        }else{
-            return dp[i][buy]=Math.max(prices[i]+helper(i+2,prices,1,dp),helper(i+1,prices,0,dp));
-        }
-    }
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
+        int dp[][] = new int[prices.length+1][2];
         for(int i=0;i<prices.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return helper(0,prices,1,dp);
+        return helper(prices,0,0,dp);
+    }
+    public int helper(int arr[],int i,int buy,int dp[][]){
+        if(i>=arr.length)return 0;
+        if(dp[i][buy]!=-1)return dp[i][buy];
+        if(buy==1){
+            return dp[i][buy]=Math.max(arr[i]+helper(arr,i+2,0,dp),helper(arr,i+1,buy,dp));
+        }
+        else{
+            return dp[i][buy]=Math.max(-arr[i]+helper(arr,i+1,1,dp),helper(arr,i+1,buy,dp));
+        }
     }
 }
