420f412c90b4a48ea27226334a1d8f8fddb2f94a
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-18 21:53:32 +0530

    Time: 0 ms (100%), Space: 42.9 MB (57.9%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void helper(TreeNode root,int sum[]){
+        if(root==null)return;
+
+        helper(root.right,sum);
+        sum[0]=sum[0]+root.val;
+        root.val = sum[0];
+        helper(root.left,sum);
+
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        int sum[]= {0};
+        helper(root,sum);
+        return root;
+    }
+}
