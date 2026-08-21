957c3d001266a24f9039520e5941e0999bafe25e
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-21 16:52:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -19,14 +19,14 @@ class Solution {
         Arrays.fill(dp,-1);
         // return helper(nums,nums.length-1,dp);
 
-        dp[0] = nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
+        // dp[0] = nums[0];
+        // dp[1]=Math.max(nums[0],nums[1]);
 
-        for(int i=2;i<dp.length;i++){
-            int pick = nums[i]+dp[i-2];
-            int not_pick = dp[i-1];
-            dp[i]=Math.max(pick,not_pick);
-        }
+        // for(int i=2;i<dp.length;i++){
+        //     int pick = nums[i]+dp[i-2];
+        //     int not_pick = dp[i-1];
+        //     dp[i]=Math.max(pick,not_pick);
+        // }
         // return dp[nums.length-1];
 
 
