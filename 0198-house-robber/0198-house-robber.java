70ce4cb3572c0a38dff1dab9c98719ab7e90ec81
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-21 16:52:41 +0530

    Time: 0 ms (100%), Space: 42.6 MB (88.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int helper(int arr[],int i,int dp[]){
-        if(i==0)return arr[i];
-        if(i==1)return Math.max(arr[i],arr[i-1]);
-        if(dp[i]!=-1)return dp[i];
+    // public int helper(int arr[],int i,int dp[]){
+    //     if(i==0)return arr[i];
+    //     if(i==1)return Math.max(arr[i],arr[i-1]);
+    //     if(dp[i]!=-1)return dp[i];
 
-        int pick = 0;
-        if(i>1){
-            pick = arr[i]+helper(arr,i-2,dp);
-        }
-        int not_pick = helper(arr,i-1,dp);
+    //     int pick = 0;
+    //     if(i>1){
+    //         pick = arr[i]+helper(arr,i-2,dp);
+    //     }
+    //     int not_pick = helper(arr,i-1,dp);
 
-        return dp[i]=Math.max(pick,not_pick);
-    }
+    //     return dp[i]=Math.max(pick,not_pick);
+    // }
     public int rob(int[] nums) {
         if(nums.length==1)return nums[0];
         if(nums.length==0)return 0;
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp,-1);
+        // int dp[] = new int[nums.length];
+        // Arrays.fill(dp,-1);
         // return helper(nums,nums.length-1,dp);
 
         // dp[0] = nums[0];
