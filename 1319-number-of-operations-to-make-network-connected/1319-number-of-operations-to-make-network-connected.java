3aff4682573e17e7d497acd2448e80a010b0369d
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-22 16:40:43 +0530

    Time: 4 ms (90.65%), Space: 65.3 MB (92.3%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -3,36 +3,38 @@ class Solution {
         int parent[];
         public DisjointSet(int n){
             parent = new int[n];
-            for(int i=0;i<n;i++)parent[i]=i;
+            for(int i=0;i<parent.length;i++){
+                parent[i]=i;
+            }
         }
 
-        public void findunion(int u,int v){
+        public void findUnion(int u,int v){
             int ulp_u = findParent(u);
             int ulp_v = findParent(v);
             if(ulp_u==ulp_v)return;
 
-            parent[ulp_u] = ulp_v;
+            parent[ulp_v]=ulp_u;
         }
 
         public int findParent(int node){
-            if(parent[node]==node)return node;
-            return parent[node] = findParent(parent[node]);
+            if(node==parent[node])return node;
+
+            return parent[node]=findParent(parent[node]);
         }
     }
     public int makeConnected(int n, int[][] connections) {
         DisjointSet ds = new DisjointSet(n);
         int z = connections.length;
         if(z<n-1)return -1;
-
         for(int i=0;i<connections.length;i++){
-            ds.findunion(connections[i][0],connections[i][1]);
+            int u =connections[i][0];
+            int v =connections[i][1];
+            ds.findUnion(u,v);
         }
-
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<n;i++){
-            set.add(ds.findParent(i));
+        int cnt=0;
+        for(int i=0;i<ds.parent.length;i++){
+            if(ds.parent[i]==i)cnt++;
         }
-
-        return set.size()-1;
+        return cnt-1;
     }
 }
