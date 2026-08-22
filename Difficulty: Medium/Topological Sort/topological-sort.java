6da7f79fd6dde2bbc7faffad3f95c4e35b2c37e3
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-08-22 11:09:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,46 +1,37 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-        int indegree[] = new int[V];
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
+        
         for(int i=0;i<edges.length;i++){
             adj.get(edges[i][0]).add(edges[i][1]);
-            indegree[edges[i][1]]++;
         }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<indegree.length;i++){
-            if(indegree[i]==0){
-                q.add(i);
+        
+        Stack<Integer> stack = new Stack<>();
+        boolean vis[] = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis,stack);
             }
         }
         ArrayList<Integer> ans = new ArrayList<>();
-        bfs(q,indegree,adj,ans);
+        while(!stack.isEmpty()){
+            ans.add(stack.pop());
+        }
+        
         return ans;
+        
     }
-    // public void dfs(int i,ArrayList<ArrayList<Integer>> adj,Stack<Integer> ans,boolean vis[]){
-    //     vis[i]=true;
-        
-    //     for(int j:adj.get(i)){
-    //         if(!vis[j]){
-    //             dfs(j,adj,ans,vis);
-    //         }
-    //     }
-        
-    //     ans.push(i);
-    // }
-    
-    public void bfs(Queue<Integer> q,int indegree[],ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ans){
-        while(!q.isEmpty()){
-            int top = q.remove();
-            ans.add(top);
-            for(int j:adj.get(top)){
-                indegree[j]--;
-                if(indegree[j]==0)
-                q.add(j);
+    public void dfs(int i,ArrayList<ArrayList<Integer>> adj,boolean vis[],Stack<Integer> stack){
+        vis[i]=true;
+        for(int j:adj.get(i)){
+            if(!vis[j]){
+                dfs(j,adj,vis,stack);
             }
         }
+        stack.push(i);
     }
 }
