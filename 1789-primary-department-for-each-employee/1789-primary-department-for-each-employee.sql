5634a55cd186595225b414b4908875d7faacda18
Ekaaanshhhh <ekaaanshhhh@gmail.com>
2026-09-06 22:09:54 +0530

    Time: 832 ms (10.13%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
 
-
-with info as(
-    select employee_id,max(primary_flag) as primary_flag
+select e.employee_id,e.department_id
+    from (
+    select employee_id,max(primary_flag) as primary_dept_flag
     from Employee
-    group by employee_id
-)
-
-select e.employee_id,e.department_id from Employee as e
-inner join info as i
-on i.employee_id=e.employee_id and i.primary_flag=e.primary_flag;
-
+    group by employee_id 
+    ) as i
+    inner join Employee as e
+    on i.employee_id = e.employee_id and e.primary_flag = i.primary_dept_flag
